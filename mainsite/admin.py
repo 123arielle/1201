5bc6ba88d77dc0c9acaadaced7bb0563106fbe85
123arielle <imariellewang@gmail.com>
2020-12-01 14:18:31 +0800

    first commit

---
+++ mainsite/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from mainsite.models import Post, AccessInfo
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(AccessInfo)
